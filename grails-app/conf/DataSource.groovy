02bc3276bd9e1b923296cb012f739cfedcc44ae4
MarcosGirardi <marcos.girardi@uccuyosl.edu.ar>
2016-06-22 10:43:16 -0300

    Merge preparado

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -4,7 +4,7 @@ dataSource {
     driverClassName = "org.h2.Driver"
     username = "sa"
     password = ""
-}
+} 
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
