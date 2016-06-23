f9f273b3ec5efb31679c4f67d4831d3967bf59f4
MarcosGirardi <marcos.girardi@uccuyosl.edu.ar>
2016-06-23 09:50:58 -0300

    Algunos cambios y agregados

--- grails-app/controllers/pruebaformulario/PruebaformularioController.groovy
+++ grails-app/controllers/pruebaformulario/PruebaformularioController.groovy
@@ -8,13 +8,16 @@ class PruebaformularioController {
     def create() {
       log.println("llega un request")
       def parametros = request.JSON
+      def formulario
+      def error
 
       try {
         log.println("validacion")
         validationService.validate(parametros)
         log.println("paso las validaciones")
       } catch (Exception v){
-
+        log.println("error en validacion")
+        error = v.getMessage()
       }
 
       try {
@@ -22,28 +25,28 @@ class PruebaformularioController {
         formularioService.guardar(parametros)
         log.println("guardado")
       } catch (Exception s){
-
+        log.println("error en guardado")
+        error = s.getMessage()
       }
 
-      if (recurso){
+      if (formulario){
+        log.println("formulario guardado")
         response.status = 201
-        Map responseMap = recursoService.getResponseMap(recurso)
-        log.println ("${responseMap}")
         request.withFormat {
           form multipartForm {
-            flash.message = message(code: 'default.created.message', args: [message(code: 'recurso.label', default: 'Recurso'), recurso.id])
-            redirect(controller: "recurso", action: "show", id: params.id)
+            flash.message = message(code: 'default.created.message', args: [message(code: 'formulario.label', default: 'Formulario'), recurso.id])
+            redirect(controller: "formulario", action: "show", id: recurso.id)
           }
-          '*' { respond recurso, [status: CREATED] }
+          '*' { respond formulario, [status: CREATED] }
           }
       } else {
-        log.println ("error....")
+        log.println ("error")
         response.status = 500
         request.withFormat {
           form multipartForm {
             flash.message = message(code: "${error}", args: [message(code: 'recurso.label', default: 'Recurso')])
             params.lastLampChange = null
-            redirect(controller: "recurso", action: "create", params: params)
+            redirect(controller: "recurso", action: "create", params: parametros)
           }
           '*' { respond recurso, [status: CREATED] }
         }
