285c5db79c7dc1123d4674e2ba23a43a0bd4a601
MarcosGirardi <marcos.girardi@uccuyosl.edu.ar>
2016-07-01 14:41:41 -0300

    Arreglo en ValidationService

--- grails-app/services/pruebaformulario/ValidationService.groovy
+++ grails-app/services/pruebaformulario/ValidationService.groovy
@@ -10,18 +10,29 @@ class ValidationService {
     log.println("validando")
     String message
 
-    if(!params.fechaNac){
-      message = message + "Falta fecha de nacimiento;"
+    if (!params.fechaNac){
+      message1 = "Falta fecha de nacimiento;"
     }
-    if(!params.genero){
-      message = message + "Falta genero;"
+    if (!params.genero){
+      message2 = "Falta genero;"
     }
-    if(!params.dni){
-      message = message + "Falta DNI;"
+    if (!params.dni){
+      message3 = "Falta DNI;"
     }
-    if(!params.correo){
-      message = message + "Falta correo"
+    if (!params.correo){
+      message4 = "Falta correo"
     }
+
+    if (message1){message = message + message1}
+    if (message2){
+      if (message){message = message + "\n" + message2}
+    }else {message = message + message2}
+    if (message3){
+      if (message){message = message + "\n" + message3}
+    } else {message = message + message3}
+    if (message4){
+      if (message){message = message + "\n" + message4}
+    }else {message = message + message4}
 
     if(message){
       throw new Exception (message)
